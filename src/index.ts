dce0b83e547f8eed79859ed5052e638e1f8d8dd0
nolawnchairs <nolawnchairs@gmail.com>
2022-09-29 20:16:02 +0700

    changed index export pattern

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,12 @@
 
-import { M3u8LineType } from './enums/m3u8-line-type'
-import { M3u8Tag } from './enums/m3u8-tag.enum'
-import { IM3u8Line } from './interfaces/m3u8-line.interface'
-import { IM3u8MediaSegment } from './interfaces/m3u8-media-segment.interface'
-import { IM3u8Producer } from './interfaces/m3u8-producer.interface'
-import { M3u8Slice } from './m3u8-slice.class'
-import { M3u8Slicer } from './m3u8-slicer.class'
-import { M3u8 } from './m3u8.class'
-import { MasterM3u8 } from './master-m3u8.class'
-import { MediaM3u8 } from './media-m3u8.class'
-import { TargetResolver } from './util/target-resolver.util'
-
-export {
-  MasterM3u8,
-  MediaM3u8,
-  M3u8Slice,
-  M3u8Slicer,
-  M3u8LineType,
-  M3u8Tag,
-  M3u8,
-  IM3u8Line,
-  IM3u8Producer,
-  IM3u8MediaSegment,
-  TargetResolver,
-}
+export type { IM3u8Line } from './interfaces/m3u8-line.interface'
+export type { IM3u8MediaSegment } from './interfaces/m3u8-media-segment.interface'
+export type { IM3u8Producer } from './interfaces/m3u8-producer.interface'
+export { M3u8LineType } from './enums/m3u8-line-type'
+export { M3u8Tag } from './enums/m3u8-tag.enum'
+export { M3u8Slice } from './m3u8-slice.class'
+export { M3u8Slicer } from './m3u8-slicer.class'
+export { M3u8 } from './m3u8.class'
+export { MasterM3u8 } from './master-m3u8.class'
+export { MediaM3u8 } from './media-m3u8.class'
+export { TargetResolver } from './util/target-resolver.util'
